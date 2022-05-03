32f6163b20226892db9955354261885dc056f182
HDLe <89816290+Dhle11@users.noreply.github.com>
2022-05-02 17:56:50 -0700

    Update BinarySearchTree.js

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+const Queue = require("./Queue");
+
 class BinarySearchTree {
   constructor(key = null, value = null, parent = null) {
     this.key = key;
@@ -91,6 +93,26 @@ class BinarySearchTree {
       values = this.right.dfsPostOrder(values);
     }
     values.push(this.values);
+    return values;
+  }
+
+  bfs(tree, values = []) {
+    const queue = new Queue();
+    queue.enqueue(tree);
+    let node = queue.dequeue();
+    while (node) {
+      values.push(node.value);
+
+      if (node.left) {
+        queue.enqueue(node.left);
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right);
+      }
+      node = queue.dequeue();
+    }
+
     return values;
   }
 
